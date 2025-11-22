7b73e801f4621ca06f71a3f52e4d531c0b5954b6
aml264 <aml264@gmail.com>
2025-11-22 14:44:55 -0500

    added some links to source data, notes, and styling

--- index.php
+++ index.php
@@ -20,18 +20,19 @@
  	</head>
 	<body>
 		<div class="container">
-			<div class="row">
+			<div class="row border mb-2">
 				<h1>The Morrowind Merchant Economy</h1>
 				<p>How much money is there in Morrowind and who has it?</p>
 				<p>Coming soon.</p>			
 			</div>
 
-			<div class="row">
+			<div class="row mb-2">
 			
-				<div class="col-sm">
-					<button onclick="fetchData(this)">Get Data</button>
-					<div class="flex">
-
+				<div class="col-sm-3 border">
+					<div class="row">
+						<button class="btn" onclick="fetchData(this)">Get Data</button>
+					</div>
+					<div class="row px-2">
 						 <label for="sortBySelectId">Show:</label><br><select id="sortBySelectId">
 							<option value="Race">Race</option>
 							<option value="Location">Location</option>
@@ -45,10 +46,27 @@
 					Include / Exclude
 					</div>				
 				</div>
-				<div class="col-lg">
-
+				<div class="col-lg-8">
 					<div id="ResultsDiv" class="row"></div>
 				</div>
+			</div>
+			<div class="row border p-1">
+				<div class="col">
+					<p>Data for this project was obtained from:<br>
+					<a href="https://en.uesp.net/wiki/Morrowind:Merchants" target="_blank">https://en.uesp.net/wiki/Morrowind:Merchants</a><br>
+					<a href="https://en.uesp.net/wiki/Tribunal:Services#Merchants" target="_blank">https://en.uesp.net/wiki/Tribunal:Services#Merchants</a><br>
+					<a href="https://en.uesp.net/wiki/Bloodmoon:Merchants" target="_blank">https://en.uesp.net/wiki/Bloodmoon:Merchants</a></p>				
+				</div>
+				<div class="col">
+					<p>Notes about the data:<br>
+					<ul>
+						<li>Todd Test and Lord Cluttermonkey are not included</li>
+						<li>All Tribunal merchant locations set to Mournhould</li>
+						<li>Both traders in Raven Rock are included even though you can only get one of them in game</li>
+					</ul>
+					</p>
+				</div>
+
 			</div>
 		</div>
 	</body>
